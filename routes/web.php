cb474deb43e0a6366a88e984c930394f33df50db
samodunlade <samodunlade@gmail.com>
2026-01-01 17:17:22 +0100

    App\Http\Controllers\site\PublicProfileController error fixed on web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Site\PageController;
-use App\Http\Controllers\site\PublicProfileController;
+use App\Http\Controllers\Site\PublicProfileController;
 
 
 
